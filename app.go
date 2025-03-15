a851c8b933ff9158c88bef3aa99f2c51f223fef0
jakubret <110852976+jakubret@users.noreply.github.com>
2025-03-15 22:18:28 +0100

    Update app.go

--- app.go
+++ app.go
@@ -5,22 +5,21 @@ import (
     "encoding/json"
     "fmt"
     "net/http"
-	
+
     "fyne.io/fyne/v2"
     "fyne.io/fyne/v2/app"
     "fyne.io/fyne/v2/container"
     "fyne.io/fyne/v2/dialog"
     "fyne.io/fyne/v2/widget"
-	
 )
 
 type SwiftCode struct {
-	Address        string `json:"address"`
-	BankName       string `json:"bankName"`
-	CountryISO2    string `json:"countryISO2"`
-	CountryName    string `json:"countryName"`
-	IsHeadquarter  bool   `json:"isHeadquarter"`
-	SwiftCode      string `json:"swiftCode" gorm:"primary_key"`
+    Address        string `json:"address"`
+    BankName       string `json:"bankName"`
+    CountryISO2    string `json:"countryISO2"`
+    CountryName    string `json:"countryName"`
+    IsHeadquarter  bool   `json:"isHeadquarter"`
+    SwiftCode      string `json:"swiftCode" gorm:"primary_key"`
 }
 
 const API_URL = "http://localhost:8080/v1/swift-codes"
@@ -52,7 +51,7 @@ func main() {
             labels[1].(*widget.Label).SetText("Adres: " + swiftCodes[i].Address)
         },
     )
-	
+
     searchButton := widget.NewButton("Wyszukaj kod SWIFT", func() {
         swiftCode := swiftCodeEntry.Text
         if swiftCode == "" {
@@ -84,62 +83,62 @@ func main() {
     })
 
     searchCountryButton := widget.NewButton("Wyszukaj kody SWIFT dla kraju", func() {
-		countryCode := countryCodeEntry.Text
-		if countryCode == "" {
-			resultLabel.SetText("Wprowadź kod kraju (ISO2)")
-			return
-		}
-	
-		resp, err := http.Get(fmt.Sprintf("%s/country/%s", API_URL, countryCode))
-		if err != nil {
-			resultLabel.SetText(fmt.Sprintf("Błąd: %v", err))
-			return
-		}
-		defer resp.Body.Close()
-	
-		if resp.StatusCode == http.StatusOK {
-			var codes []SwiftCode
-			if err := json.NewDecoder(resp.Body).Decode(&codes); err != nil {
-				resultLabel.SetText(fmt.Sprintf("Błąd dekodowania: %v", err))
-				return
-			}
-	
-			resultWindow := myApp.NewWindow("Wyniki wyszukiwania")
-			resultWindow.Resize(fyne.NewSize(800, 600)) 
-	
-			resultList := widget.NewList(
-				func() int {
-					return len(codes)
-				},
-				func() fyne.CanvasObject {
-					return container.NewVBox(
-						widget.NewLabel("Bank:"),
-						widget.NewLabel("Adres:"),
-						widget.NewLabel("Kraj:"),
-						widget.NewLabel("Główna siedziba:"),
-					)
-				},
-				func(i widget.ListItemID, o fyne.CanvasObject) {
-					box := o.(*fyne.Container)
-					labels := box.Objects
-					labels[0].(*widget.Label).SetText("Bank: " + codes[i].BankName)
-					labels[1].(*widget.Label).SetText("Adres: " + codes[i].Address)
-					labels[2].(*widget.Label).SetText("Kraj: " + codes[i].CountryName)
-					labels[3].(*widget.Label).SetText("Główna siedziba: " + fmt.Sprintf("%v", codes[i].IsHeadquarter))
-				},
-			)
-	
-			resultWindow.SetContent(container.NewBorder(
-				widget.NewLabel(fmt.Sprintf("Wyniki wyszukiwania dla kraju: %s", countryCode)),
-				nil, nil, nil,
-				container.NewVScroll(resultList),
-			))
-	
-			resultWindow.Show()
-		} else {
-			resultLabel.SetText("Nie znaleziono kodów SWIFT dla tego kraju")
-		}
-	})
+        countryCode := countryCodeEntry.Text
+        if countryCode == "" {
+            resultLabel.SetText("Wprowadź kod kraju (ISO2)")
+            return
+        }
+
+        resp, err := http.Get(fmt.Sprintf("%s/country/%s", API_URL, countryCode))
+        if err != nil {
+            resultLabel.SetText(fmt.Sprintf("Błąd: %v", err))
+            return
+        }
+        defer resp.Body.Close()
+
+        if resp.StatusCode == http.StatusOK {
+            var codes []SwiftCode
+            if err := json.NewDecoder(resp.Body).Decode(&codes); err != nil {
+                resultLabel.SetText(fmt.Sprintf("Błąd dekodowania: %v", err))
+                return
+            }
+
+            resultWindow := myApp.NewWindow("Wyniki wyszukiwania")
+            resultWindow.Resize(fyne.NewSize(800, 600))
+
+            resultList := widget.NewList(
+                func() int {
+                    return len(codes)
+                },
+                func() fyne.CanvasObject {
+                    return container.NewVBox(
+                        widget.NewLabel("Bank:"),
+                        widget.NewLabel("Adres:"),
+                        widget.NewLabel("Kraj:"),
+                        widget.NewLabel("Główna siedziba:"),
+                    )
+                },
+                func(i widget.ListItemID, o fyne.CanvasObject) {
+                    box := o.(*fyne.Container)
+                    labels := box.Objects
+                    labels[0].(*widget.Label).SetText("Bank: " + codes[i].BankName)
+                    labels[1].(*widget.Label).SetText("Adres: " + codes[i].Address)
+                    labels[2].(*widget.Label).SetText("Kraj: " + codes[i].CountryName)
+                    labels[3].(*widget.Label).SetText("Główna siedziba: " + fmt.Sprintf("%v", codes[i].IsHeadquarter))
+                },
+            )
+
+            resultWindow.SetContent(container.NewBorder(
+                widget.NewLabel(fmt.Sprintf("Wyniki wyszukiwania dla kraju: %s", countryCode)),
+                nil, nil, nil,
+                container.NewVScroll(resultList),
+            ))
+
+            resultWindow.Show()
+        } else {
+            resultLabel.SetText("Nie znaleziono kodów SWIFT dla tego kraju")
+        }
+    })
 
     addSwiftCodeButton := widget.NewButton("Dodaj nowy kod SWIFT", func() {
         swiftCodeEntry := widget.NewEntry()
@@ -225,6 +224,8 @@ func main() {
 
                 if resp.StatusCode == http.StatusOK {
                     dialog.ShowInformation("Sukces", "Kod SWIFT został usunięty", myWindow)
+                } else if resp.StatusCode == http.StatusNotFound {
+                    dialog.ShowError(fmt.Errorf("Nie znaleziono kodu SWIFT"), myWindow)
                 } else {
                     dialog.ShowError(fmt.Errorf("Nie udało się usunąć kodu SWIFT"), myWindow)
                 }
